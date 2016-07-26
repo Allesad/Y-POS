79d34b05ffe0174879694b3be815e49ede264419
Allesad <allesad.dev@gmail.com>
2016-07-26 22:27:41 +0300

    Checkout Payment Complete page
    - Added CheckoutPaymentComplete page;
    - Added some temp logic to switch between states on checkout page;

---
+++ Y-POS/Views/CheckoutParts/CheckoutPaymentCompleteView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Y_POS.Views.CheckoutParts
+{
+    /// <summary>
+    /// Interaction logic for CheckoutPaymentCompleteView.xaml
+    /// </summary>
+    public partial class CheckoutPaymentCompleteView : UserControl
+    {
+        public CheckoutPaymentCompleteView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Y-POS/Views/CheckoutView.xaml.cs
+++ Y-POS/Views/CheckoutView.xaml.cs
@@ -10,6 +10,8 @@ namespace Y_POS.Views
     /// </summary>
     public partial class CheckoutView : UserControl
     {
+        private bool _isPaid;
+
         public CheckoutView()
         {
             InitializeComponent();
@@ -71,6 +73,28 @@ namespace Y_POS.Views
                 Title = title;
                 Content = content;
             }
+        }
+
+        private void SwitchActionBar(object sender, RoutedEventArgs e)
+        {
+            if (!_isPaid) return;
+
+            ActionBarLeftContainer.SetValue(Grid.ColumnSpanProperty, 1);
+            Content.Content = new CheckoutPaymentView();
+            RightActionButton.Title = Core.Properties.Resources.Void.ToUpper();
+            PaymentTypesContainer.IsEnabled = true;
+            OperationsList.IsEnabled = true;
+        }
+
+        private void Content_OnCheckout(object sender, RoutedEventArgs e)
+        {
+            ActionBarLeftContainer.SetValue(Grid.ColumnSpanProperty, 2);
+            Content.Content = new CheckoutPaymentCompleteView();
+            RightActionButton.Title = Core.Properties.Resources.Refund.ToUpper();
+            PaymentTypesContainer.IsEnabled = false;
+            OperationsList.IsEnabled = false;
+
+            _isPaid = true;
         }
     }
 }
