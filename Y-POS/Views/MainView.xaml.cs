0a81fb01ef4f57a08a1e47787928c9e9033fcbf0
Allesad <allesad.dev@gmail.com>
2016-09-07 21:34:07 +0300

    Small cleanup

--- Y-POS/Views/MainView.xaml.cs
+++ Y-POS/Views/MainView.xaml.cs
@@ -21,44 +21,6 @@ namespace Y_POS.Views
             dlgManager.SetHost(this);
         }
 
-        private void CommandBinding_OnNavigateToPage(object sender, ExecutedRoutedEventArgs e)
-        {
-            /*var target = e.Parameter as string;
-            if (string.IsNullOrEmpty(target)) return;
-
-            switch (target)
-            {
-                case "Login":
-                    Page.Content = new LoginView();
-                    break;
-                case "PinLogin":
-                    Page.Content = new PinLoginView();
-                    break;
-                case "ActiveOrders":
-                    Page.Content = new ActiveOrdersView();
-                    break;
-                case "ClosedOrders":
-                    Page.Content = new ClosedOrdersView();
-                    break;
-                case "OrderMaker":
-                    Page.Content = new OrderMakerView();
-                    break;
-                case "Checkout":
-                    Page.Content = new CheckoutView();
-                    break;
-                case "Cashdrawer":
-                    Page.Content = new CashDrawerView();
-                    break;
-                case "Reports":
-                    Page.Content = new ReportsView();
-                    break;
-                case "Settings":
-                    Page.Content = new SettingsView();
-                    break;
-            }
-            NavMenuContainer.Visibility = Visibility.Collapsed;*/
-        }
-
         public void ShowDialog(IBaseDialog dialog)
         {
             MessageBox.Show(((TextDialogContent) dialog.Content).Message);
--- Y-POS/Views/NavMenuView.xaml.cs
+++ Y-POS/Views/NavMenuView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows.Controls;
 using YumaPos.Client.Navigation;
 using Y_POS.Core;
 using Y_POS.Core.ViewModels;
@@ -20,33 +18,6 @@ namespace Y_POS.Views
         private INavMenuVm ViewModel
         {
             get { return (INavMenuVm) DataContext; }
-        }
-
-        private void NavMenuListView_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            if (NavMenuListView.SelectedItem == null) return;
-
-            switch (NavMenuListView.SelectedIndex)
-            {
-                case 0:
-                    NavigationCommands.GoToPage.Execute("ActiveOrders", (IInputElement) e.Source);
-                    break;
-                case 1:
-                    NavigationCommands.GoToPage.Execute("ClosedOrders", (IInputElement) e.Source);
-                    break;
-                case 2:
-                    NavigationCommands.GoToPage.Execute("Cashdrawer", (IInputElement) e.Source);
-                    break;
-                case 3:
-                    NavigationCommands.GoToPage.Execute("Reports", (IInputElement) e.Source);
-                    break;
-                case 4:
-                    NavigationCommands.GoToPage.Execute("Settings", (IInputElement) e.Source);
-                    break;
-                case 5:
-                    NavigationCommands.GoToPage.Execute("PinLogin", (IInputElement) e.Source);
-                    break;
-            }
         }
 
         private void NavMenuListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
